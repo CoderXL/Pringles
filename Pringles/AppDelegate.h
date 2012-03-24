eceda9e3e96f8efc74e85516aedb48131c89ed96
li hao <lihao@limatoMacBook-Pro.local>
2012-03-25 01:14:34 +0800

    Initial Commit

---
+++ Pringles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pringles
+//
+//  Created by hao li on 12-3-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pringles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pringles
+//
+//  Created by hao li on 12-3-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
